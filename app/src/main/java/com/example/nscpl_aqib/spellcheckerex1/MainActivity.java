a9ad919f911e97ba9603449466160a1882823eae
mirzaaqib <mirzaaqibbeg@gmail.com>
2017-02-22 15:13:02 +0530

    SpellChecker Complete code Example

---
+++ app/src/main/java/com/example/nscpl_aqib/spellcheckerex1/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.nscpl_aqib.spellcheckerex1;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.textservice.SentenceSuggestionsInfo;
+import android.view.textservice.SpellCheckerSession;
+import android.view.textservice.SuggestionsInfo;
+import android.view.textservice.TextInfo;
+import android.view.textservice.TextServicesManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SpellCheckerSession.SpellCheckerSessionListener{
+    Button b1;
+    EditText ed1;
+    TextView tv1;
+   private SpellCheckerSession mScs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1= (TextView) findViewById(R.id.textView3);
+        b1= (Button) findViewById(R.id.button);
+        ed1= (EditText) findViewById(R.id.editText);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),ed1.getText().toString(), Toast.LENGTH_SHORT).show();
+                mScs.getSuggestions(new TextInfo(ed1.getText().toString()),5);
+                ed1.setText("");
+                
+            }
+        });
+    }
+    public void onResume(){
+        super.onResume();
+        final TextServicesManager tsm= (TextServicesManager) getSystemService(Context.TEXT_SERVICES_MANAGER_SERVICE);
+        mScs=tsm.newSpellCheckerSession(null,null,this,true);
+    }
+    public void onPause(){
+        super.onPause();
+        if(mScs!=null){
+            mScs.close();
+        }
+    }
+
+    @Override
+    public void onGetSuggestions(SuggestionsInfo[] arg0) {
+        final StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arg0.length; ++i) {
+            // Returned suggestions are contained in SuggestionsInfo
+            final int len = arg0[i].getSuggestionsCount();
+            sb.append('\n');
+
+            for (int j = 0; j < len; ++j) {
+                sb.append("," + arg0[i].getSuggestionAt(j));
+            }
+
+            sb.append(" (" + len + ")");
+        }
+
+        runOnUiThread(new Runnable() {
+            public void run() {
+                tv1.append(sb.toString());
+            }
+        });
+
+    }
+
+    @Override
+    public void onGetSentenceSuggestions(SentenceSuggestionsInfo[] results) {
+
+    }
+}
